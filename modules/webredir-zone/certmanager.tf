aa4e1a0977b3c3574703bc89ac979dff5b437a89
Michael McKinnon <michael@mckinnon.id.au>
2022-09-19 17:24:36 +1000

    Better DNS validation records and dependency

--- modules/webredir-zone/certmanager.tf
+++ modules/webredir-zone/certmanager.tf
@@ -11,29 +11,34 @@ resource "aws_acm_certificate" "certificate" {
   ]
 }
 
-# Special validation for getting certificates from AWS
-# creates validation records in route53 for domain
-resource "aws_route53_record" "validate_record" {
-  for_each = {
-  for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
-  }
+# Create a DNS record for validation of example.com
+resource "aws_route53_record" "root_validation_record" {
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = var.zone_id
+  zone_id = data.aws_route53_zone.domain.zone_id
+  ttl = 60
+  type = tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_type
+  name = tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_name
+  records = [ tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_value ]
+}
+
+# Create a DNS record for validation of www.example.com
+resource "aws_route53_record" "www_validation_record" {
+  allow_overwrite = true
+  zone_id = data.aws_route53_zone.domain.zone_id
+  ttl = 60
+  type = tolist(aws_acm_certificate.certificate.domain_validation_options)[1].resource_record_type
+  name = tolist(aws_acm_certificate.certificate.domain_validation_options)[1].resource_record_name
+  records = [ tolist(aws_acm_certificate.certificate.domain_validation_options)[1].resource_record_value ]
 }
 
 # Performs the DNS based validation by creating temporary
 # DNS entries for a delegated domain
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validate_record : record.fqdn]
+  validation_record_fqdns = [
+    aws_route53_record.root_validation_record.fqdn,
+    aws_route53_record.www_validation_record.fqdn
+  ]  
 }
 
 # Find the Port 443 listener on the load balancer
@@ -43,6 +48,7 @@ data "aws_lb_listener" "listener" {
 
 # Need to add the certificate to the loadbalancer
 resource "aws_lb_listener_certificate" "bindcert" {
+  depends_on = [aws_acm_certificate_validation.validation]
   certificate_arn = aws_acm_certificate.certificate.arn
   listener_arn = data.aws_lb_listener.listener.arn
 }
